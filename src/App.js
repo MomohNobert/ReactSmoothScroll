ed2f1222266db1fea583a2b63a542e6f58c7cee5
MomohNobert <okhai0112@gmail.com>
2020-09-24 15:30:24 +0100

    added new classes and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import About from './components/about/about.component';
+import Contact from './components/contact/contact.component';
+import Main from './components/main/main.component';
+import Navbar from './components/nav-bar/nav-bar.component';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Main />
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/main/main.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="main-section">
+
+        </div>
+    )
+};
+
+export default Main
